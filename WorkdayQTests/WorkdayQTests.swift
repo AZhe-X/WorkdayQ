4276f63850125339ab927ce773ed5e2900b3ace4
Xueqi Li <el@xueqi.li>
2025-03-03 02:54:45 -0800

    Initial Commit

---
+++ WorkdayQTests/WorkdayQTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkdayQTests.swift
+//  WorkdayQTests
+//
+//  Created by Xueqi Li on 3/3/25.
+//
+
+import Testing
+@testable import WorkdayQ
+
+struct WorkdayQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
